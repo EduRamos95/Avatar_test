1b6c1e5ce27aec1db59db32f50da0018b8949824
EduRamos95 <eduramos08.30@gmail.com>
2023-11-12 03:34:03 -0500

    feat: add list movies

--- frontend/avatar_react/src/App.tsx
+++ frontend/avatar_react/src/App.tsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import CircularProgress from '@mui/material/CircularProgress';
+import MoviesList from './Components/list-movies/MoviesList';
+import { FilmDetailDto } from './Models/MsStarwars/Response/StarwarsControllerV1';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [films, setFilms] = useState<FilmDetailDto[]>([]);
+  const [selectedFilm, setSelectedFilm] = useState(null);
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setFilms(data.results);
+      setStatus(true);
+    } catch (error) {
+      console.error('Error fetching films:', error);
+      setStatus(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // const fetchFilmDetails = async (filmUrl) => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await fetch(filmUrl);
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+  //     const filmDetails = await response.json();
+  //     setSelectedFilm(filmDetails);
+  //     // Fetch details of characters in the film
+  //     const charactersData = await Promise.all(
+  //       filmDetails.characters.map(async (characterUrl) => {
+  //         const characterResponse = await fetch(characterUrl);
+  //         if (!characterResponse.ok) {
+  //           throw new Error('Network response was not ok');
+  //         }
+  //         const characterData = await characterResponse.json();
+  //         return characterData.name;
+  //       })
+  //     );
+  //     setCharacters(charactersData);
+  //   } catch (error) {
+  //     console.error('Error fetching film details:', error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  const handleButtonClick = () => {
+    // Llamamos a la función para cargar los datos al hacer clic en el botón
+    if (films.length > 0) return;
+    fetchData();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='ContainerPage'>
+        <h1>Star Wars Films</h1>
+        <button onClick={handleButtonClick}>
+          {loading ? <CircularProgress size={20} style={{ marginRight: 10 }} /> : null}
+          {loading ? 
+            <span>Loading...</span> 
+            : status ? 
+              <span>Load Films</span> :
+              <span>Search Films</span> 
+          }
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className='Cards'>
+        { films.map((film, index) => 
+          <MoviesList  key={index} {...film} />
+          )
+        }
+        </div>
+        {/* 
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <ul>
+              {films.map((film) => (
+                <li key={film.title}>
+                  {film.title}
+                  <button onClick={() => fetchFilmDetails(film.url)}>View Details</button>
+                </li>
+              ))}
+            </ul>
+            {selectedFilm && (
+              <div>
+                <h2>Film Details</h2>
+                <p>Title: {selectedFilm.title}</p>
+                <p>Episode ID: {selectedFilm.episode_id}</p>
+                <p>Director: {selectedFilm.director}</p>
+                <p>Producer: {selectedFilm.producer}</p>
+                <p>Release Date: {selectedFilm.release_date}</p>
+                <h3>Characters</h3>
+                <ul>
+                  {characters.map((character, index) => (
+                    <li key={index}>{character}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </>
+        )}
+      */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
